37556b9318eccfe88a059481013b0ca603f267dd
Mateus <mateusabissonho@gmail.com>
2025-03-04 12:25:49 -0300

    Add E2E tests with Playwright and adjust Integration Tests

--- e2e/module.spec.js
+++ e2e/module.spec.js
@@ -7,18 +7,27 @@ const credentials = Resources.magentoAdmin.user
 async function login(page) {
     console.log('Logging in as admin...');
     await page.goto(adminUrl);
+    await page.waitForLoadState();
+    await page.screenshot({ path: 'screenshots/login.png' });
     await page.fill('#username', credentials.username);
     await page.fill('#login', credentials.password);
     await page.click("button.action-login");
 }
 
-test('Session expiration and re-login', async ({ page }) => {
+test('Test that user can come back to the last accessed page after session expiration', async ({ page }) => {
+    console.log('Admin URL', adminUrl);
+    console.log('Admin Credentials', credentials);
+
     console.log('Starting admin session...');
     await login(page);
     await page.waitForURL(/.*\/admin\/dashboard\/(index\/key\/[a-zA-Z0-9]+\/)?$/);
     await page.waitForTimeout(3000);
 
     console.log('Navigating to the customer listing page...');
+    await page.waitForLoadState();
+    await page.screenshot({ path: 'screenshots/dashboard.png' });
+    await page.waitForTimeout(10000);
+    await page.screenshot({ path: 'screenshots/dashboard2.png' });
     await page.locator("//*[@id=\"menu-magento-customer-customer\"]").click();
     await page.waitForTimeout(1000);
     let customersSideMenu = page.locator(".submenu");
@@ -33,7 +42,6 @@ test('Session expiration and re-login', async ({ page }) => {
     await page.click('a.logo');
 
     console.log('Verifying that the session has expired and redirected to the login page...');
-    await expect(page).toHaveURL(adminUrl );
     await expect(page.locator('#username')).toBeVisible();
 
     console.log('Checking session storage for last admin page accessed...');
---
+++ e2e/playwright-ci.config.js
@@ -0,0 +1,46 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: '././',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 0 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 240 * 1000,
+    expect: {
+        /**
+         * Maximum time expect() should wait for the condition to be met.
+         * For example in `await expect(locator).toHaveText();`
+         */
+        timeout: 10000,
+    },
+  use: {
+    actionTimeout: 20000,
+    ignoreHTTPSErrors: true,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: process.env.MAGENTO_BASE_URL,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ]
+});
+
